4f9560260156a10e9c3e5da1768d894bcf06bdd8
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-02-06 09:48:29 +0100

    Change Role model and rename Characters file to Character

--- src/model/Role.kt
+++ src/model/Role.kt
@@ -3,11 +3,9 @@ package com.kaiserpudding.model
 import org.jetbrains.exposed.sql.Table
 
 object Roles: Table() {
-    val id = integer("id").primaryKey().autoIncrement()
-    val name = varchar("name", 15)
+    val name = varchar("name", 15).primaryKey()
 }
 
 data class Role(
-    val id: Int,
     val name: String
 )
