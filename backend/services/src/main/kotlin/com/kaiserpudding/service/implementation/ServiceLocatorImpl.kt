920d0b60e195eb6274a5dbd326c3bedcc6da5f76
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-05-30 20:21:57 +0200

    clean up

--- backend/services/src/main/kotlin/com/kaiserpudding/service/implementation/ServiceLocatorImpl.kt
+++ backend/services/src/main/kotlin/com/kaiserpudding/service/implementation/ServiceLocatorImpl.kt
@@ -1,19 +1,10 @@
 package com.kaiserpudding.service.implementation
 
-import com.kaiserpudding.repository.CharacterRepository
-import com.kaiserpudding.repository.PartyRepository
-import com.kaiserpudding.repository.RoleRepository
-import com.kaiserpudding.repository.SkillInfoRepository
-import com.kaiserpudding.repository.SkillRepository
-import com.kaiserpudding.service.CharacterService
-import com.kaiserpudding.service.PartyService
-import com.kaiserpudding.service.RoleService
-import com.kaiserpudding.service.ServiceLocator
-import com.kaiserpudding.service.SkillInfoService
-import com.kaiserpudding.service.SkillService
+import com.kaiserpudding.repository.*
+import com.kaiserpudding.service.*
 
 internal class ServiceLocatorImpl : ServiceLocator {
-    override val characterService: CharacterService = CharacterServiceImpl(CharacterRepository(), SkillRepository())
+    override val characterService: CharacterService = CharacterServiceImpl(CharacterRepository())
     override val partyService: PartyService = PartyServiceImpl(PartyRepository())
     override val roleService: RoleService = RoleServiceImpl(RoleRepository())
     override val skillInfoService: SkillInfoService = SkillInfoServiceImpl(SkillInfoRepository())
