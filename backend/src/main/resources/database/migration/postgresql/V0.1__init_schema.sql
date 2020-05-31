ca7200c6392a635a03379d5e59d09e25d36699e8
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-05-31 16:42:45 +0200

    change role_name to role. remove skills.id column. replace unique for pk

--- backend/src/main/resources/database/migration/postgresql/V0.1__init_schema.sql
+++ backend/src/main/resources/database/migration/postgresql/V0.1__init_schema.sql
@@ -1,58 +1,68 @@
 CREATE TABLE roles (
-    name varchar(15) NOT NULL,
-    CONSTRAINT roles_pkey PRIMARY KEY (name)
+    name VARCHAR(15) NOT NULL,
+    CONSTRAINT roles_PK PRIMARY KEY (name)
 );
 
 CREATE TABLE skill_infos (
-    id          int4         NOT NULL,
-    role_name   varchar(15)  NOT NULL,
-    name        varchar(30)  NOT NULL,
-    description varchar(400) NOT NULL,
-    max_level   int4         NOT NULL,
-    CONSTRAINT skill_infos_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_skill_infos_role_name_name FOREIGN KEY (role_name) REFERENCES roles (name) ON UPDATE CASCADE ON DELETE RESTRICT
+    id          INT4         NOT NULL,
+    role        VARCHAR(15)  NOT NULL,
+    name        VARCHAR(30)  NOT NULL,
+    description VARCHAR(400) NOT NULL,
+    max_level   INT4         NOT NULL,
+    CONSTRAINT skill_infos_PK PRIMARY KEY (id),
+    CONSTRAINT skill_infos__role_FK FOREIGN KEY (role) REFERENCES roles (name) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE skill_info_prerequisites (
-    skill_info_id      int4 NOT NULL,
-    prerequisite_id    int4 NOT NULL,
-    prerequisite_level int4 NOT NULL,
-    CONSTRAINT fk_skill_info_prerequisites_prerequisite_id_id FOREIGN KEY (prerequisite_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_skill_info_prerequisites_skill_info_id_id FOREIGN KEY (skill_info_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE CASCADE
+    skill_info_id      INT4 NOT NULL,
+    prerequisite_id    INT4 NOT NULL,
+    prerequisite_level INT4 NOT NULL,
+    CONSTRAINT skill_info_prerequisites__skill_info_id__prerequisite_id_PK PRIMARY KEY (skill_info_id, prerequisite_id),
+    CONSTRAINT skill_info_prerequisites__prerequisite_id_FK FOREIGN KEY (prerequisite_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT skill_info_prerequisites__skill_info_id_FK FOREIGN KEY (skill_info_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE INDEX skill_info_prerequisites_skill_info_id ON skill_info_prerequisites USING btree (skill_info_id);
+CREATE INDEX skill_info_prerequisites__skill_info_id_IX ON skill_info_prerequisites USING btree (skill_info_id);
+
+CREATE TABLE users (
+    id SERIAL NOT NULL,
+    CONSTRAINT users__id_PK PRIMARY KEY (id)
+);
 
 CREATE TABLE characters (
-    id   serial      NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    role VARCHAR(15) NOT NULL,
-    CONSTRAINT characters_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_characters_role_name FOREIGN KEY (role) REFERENCES roles (name) ON UPDATE CASCADE ON DELETE RESTRICT
+    id      SERIAL      NOT NULL,
+    name    VARCHAR(30) NOT NULL,
+    role    VARCHAR(15) NOT NULL,
+    user_id INT4        NOT NULL,
+    CONSTRAINT characters_PK PRIMARY KEY (id),
+    CONSTRAINT characters__role_FK FOREIGN KEY (role) REFERENCES roles (name) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT characters__user_id_FK FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
+
 );
 
 CREATE TABLE skills (
-    id            serial NOT NULL,
-    skill_info_id INT4   NOT NULL,
-    level         INT4   NOT NULL,
-    character_id  INT4   NOT NULL,
-    CONSTRAINT skills_skill_info_id_character_id UNIQUE (skill_info_id, character_id),
-    CONSTRAINT fk_skills_character_id_id FOREIGN KEY (character_id) REFERENCES characters (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_skills_skill_info_id_id FOREIGN KEY (skill_info_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE RESTRICT
+    skill_info_id INT4 NOT NULL,
+    level         INT4 NOT NULL,
+    character_id  INT4 NOT NULL,
+    CONSTRAINT skills__skill_info_id__character_id_PK PRIMARY KEY (skill_info_id, character_id),
+    CONSTRAINT skills__character_id_FK FOREIGN KEY (character_id) REFERENCES characters (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT skills__skill_info_id_FK FOREIGN KEY (skill_info_id) REFERENCES skill_infos (id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE parties (
-    id   serial      NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    CONSTRAINT parties_id_unique UNIQUE (id)
+    id      SERIAL      NOT NULL,
+    name    VARCHAR(20) NOT NULL,
+    user_id INT4        NOT NULL,
+    CONSTRAINT parties__id_PK PRIMARY KEY (id),
+    CONSTRAINT parties__user_id_FK FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE party_members (
     party_id  INT4        NOT NULL,
     member_id INT4        NOT NULL,
     position  VARCHAR(15) NOT NULL,
-    CONSTRAINT party_members_party_id_member_id_unique UNIQUE (party_id, member_id),
-    CONSTRAINT party_members_party_id_position_unique UNIQUE (party_id, position) DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT fk_party_members_member_id_id FOREIGN KEY (member_id) REFERENCES characters (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_party_members_party_id_id FOREIGN KEY (party_id) REFERENCES parties (id) ON UPDATE CASCADE ON DELETE CASCADE
+    CONSTRAINT party_members__party_id__member_id_PK PRIMARY KEY (party_id, member_id),
+    CONSTRAINT party_members__party_id__position_UX UNIQUE (party_id, position) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT party_members__member_id_FK FOREIGN KEY (member_id) REFERENCES characters (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT party_members__party_id_FK FOREIGN KEY (party_id) REFERENCES parties (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE INDEX party_members_party_id ON party_members USING btree (party_id);
+CREATE INDEX party_members__party_id_IX ON party_members USING btree (party_id);
