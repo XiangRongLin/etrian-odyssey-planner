f63a88407b793e05de0ed01044001216f63512b1
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-06-01 21:05:39 +0200

    Adjust controllers to jwtId Field with dummy jwtId

--- backend/web/src/main/kotlin/com/kaiserpudding/api/userdata/PartyController.kt
+++ backend/web/src/main/kotlin/com/kaiserpudding/api/userdata/PartyController.kt
@@ -24,19 +24,19 @@ fun Route.party(serviceLocator: ServiceLocator) {
 
         post("/") {
             val party: NewParty = call.receive()
-            call.respond(HttpStatusCode.Created, serviceLocator.partyService.create(party, 1))//TODO
+            call.respond(HttpStatusCode.Created, serviceLocator.partyService.create(party, "1"))//TODO
         }
 
         put("/{id}") {
             val id = checkNotNull(call.parameters["id"]).toInt()
             val party: NewParty = call.receive()
-            serviceLocator.partyService.update(PartySummary(id, party), 1)//TODO
+            serviceLocator.partyService.update(PartySummary(id, party), "1")//TODO
             call.respond(HttpStatusCode.OK)
         }
 
         delete("/{id}") {
             val id = checkNotNull(call.parameters["id"]).toInt()
-            check(serviceLocator.partyService.delete(id, 1))//TODO
+            check(serviceLocator.partyService.delete(id, "1"))//TODO
             call.respond(HttpStatusCode.OK)
         }
 
@@ -44,7 +44,7 @@ fun Route.party(serviceLocator: ServiceLocator) {
             patch("/") {
                 val partyId = checkNotNull(call.parameters["id"]).toInt()
                 val members = call.receive<List<NewPartyMember>>()
-                serviceLocator.partyService.updateMembers(partyId, members, 1)//TODO
+                serviceLocator.partyService.updateMembers(partyId, members, "1")//TODO
                 call.respond(HttpStatusCode.OK)
             }
         }
