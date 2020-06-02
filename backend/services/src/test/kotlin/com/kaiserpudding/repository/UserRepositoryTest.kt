645b971dd3c6b4043f3c20b89763297ef713cf24
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-06-02 20:27:54 +0200

    Add UserRepositoryTest

--- backend/services/src/test/kotlin/com/kaiserpudding/repository/UserRepositoryTest.kt
+++ backend/services/src/test/kotlin/com/kaiserpudding/repository/UserRepositoryTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class UserRepositoryTest : AbstractRepositoryTest() {
@@ -17,5 +18,26 @@ internal class UserRepositoryTest : AbstractRepositoryTest() {
 
         assertTrue(actual != null)
         assertEquals(userId, actual!!.id)
+    }
+
+    @Test
+    fun `get(), non-existent`() = dbTest {
+        val actual = repository.get(-1)
+
+        assertTrue(actual == null)
+    }
+
+    @Test
+    fun resolveId() = dbTest {
+        val actual = repository.resolveId("jwt")
+
+        assertEquals(user.id, actual)
+    }
+
+    @Test
+    fun `resolveId(), non-existent`() = dbTest {
+        assertThrows<NoSuchElementException> {
+            repository.resolveId("abcdefg")
+        }
     }
 }
