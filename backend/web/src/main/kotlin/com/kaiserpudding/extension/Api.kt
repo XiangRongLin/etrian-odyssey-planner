9cee3f63cc9023942db3298bc7a809974119cbea
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-06-02 20:49:57 +0200

    Add extension function to retrieve subject of jwt token

--- backend/web/src/main/kotlin/com/kaiserpudding/extension/Api.kt
+++ backend/web/src/main/kotlin/com/kaiserpudding/extension/Api.kt
@@ -1,7 +1,14 @@
 package com.kaiserpudding.extension
 
 import io.ktor.application.ApplicationCall
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.principal
 
 fun ApplicationCall.getIntParameter(name: String): Int = checkNotNull(this.parameters[name]).toInt()
 fun ApplicationCall.getStringParameter(name: String): String = checkNotNull(this.parameters[name])
+fun ApplicationCall.jwtSubject(): String {
+    val principal = principal<JWTPrincipal>()
+        ?: throw IllegalStateException("Method was not called inside a jwt authentication block")
+    return principal.payload.subject
+}
 
